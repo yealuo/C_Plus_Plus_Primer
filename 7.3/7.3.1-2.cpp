088f21f2d5afb728da7f0a498bc5df7e25c45894
Yealuo <19807116985@163.com>
2023-11-13 19:35:42 +0800

    7.3.2

--- 7.3/7.3.1-2.cpp
+++ 7.3/7.3.1-2.cpp
@@ -10,14 +10,40 @@ class Screen {
         : width(wid), height(hei), contents(wid * hei, ' ') {}
     Screen(int wid, int hei, char c)
         : width(wid), height(hei), contents(wid * hei, c) {}
+
     char getchar() const { return contents[position]; }
+
+    Screen& move(int row, int column) {
+        position = row * width + column;
+        return *this;
+    }
+
+    Screen& set(char c) {
+        contents[position] = c;
+        return *this;
+    }
+
+    Screen& display(ostream& os) {
+        doDisplay(os);
+        return *this;
+    }
+    const Screen& display(ostream& os) const {
+        doDisplay(os);
+        return *this;
+    }
 
    private:
     string contents;
     length position = 0;
     length height = 0, width = 0;
+    void doDisplay(ostream& os) const { os << contents; }
 };
 
 int main() {
+    Screen myScreen(5,5,'X');
+    myScreen.move(4,0).set('#').display(cout);
+    cout<<"\n";
+    myScreen.display(cout);
+    cout<<"\n";
     return 0;
 }
